4c05459db501c3248381ae098295c8b09d13ca83
André <andrerrr@outlook.de>
2019-05-17 11:30:10 +0200

    EF DataProject von .net Standard auf .NET Framewokr backported

---
+++ ppedv.FlyingPluto/ppedv.FlyingPluto.Data.EFdotnet/EfKundenRepository.cs
@@ -0,0 +1,26 @@
+﻿using ppedv.FlyingPluto.Model;
+using ppedv.FlyingPluto.Model.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace ppedv.FlyingPluto.Data.EF
+{
+    public class EfKundenRepository : EfRepository<Kunde>, IKundenRepository
+    {
+        public EfKundenRepository(EfContext context) : base(context)
+        { }
+
+        public override void Add(Kunde entity)
+        {
+            base.Add(entity);
+            //todo besser Add
+        }
+
+        public IEnumerable<Kunde> GetBlaBlaKunden(int tage, DateTime datum)
+        {
+            //todo call StoredProc
+            //context.Database.ExecuteSqlCommand();
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ppedv.FlyingPluto/ppedv.FlyingPluto.Data.EFdotnet/EfRepository.cs
@@ -0,0 +1,54 @@
+﻿using ppedv.FlyingPluto.Model;
+using ppedv.FlyingPluto.Model.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ppedv.FlyingPluto.Data.EF
+{
+    public class EfRepository<T> : IRepository<T> where T : Entity
+    {
+        protected EfContext context;
+        public EfRepository(EfContext context)
+        {
+            this.context = context;
+        }
+
+        public virtual void Add(T entity)
+        {
+            //    if (typeof(T) == typeof(Auto))
+            //        context.Autos.Add(entity as Auto);
+            context.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            context.Set<T>().Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public IQueryable<T> Query()
+        {
+            return context.Set<T>();
+        }
+
+
+        public void Update(T entity)
+        {
+            var loaded = GetById(entity.Id);
+            if (loaded != null)
+            {
+                context.Entry(loaded).CurrentValues.SetValues(entity);
+            }
+        }
+    }
+}
